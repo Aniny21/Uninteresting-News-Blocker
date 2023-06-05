4a15a845f35c51e7913de1439349cc8cde7999bb
Aniny <75681502+Aniny21@users.noreply.github.com>
2023-06-06 02:14:52 +0900

    updated

--- Firefox/options/options.js
+++ Firefox/options/options.js
@@ -1,5 +1,18 @@
 "use strict";
 
+// jsonが正しいかどうかを判定する
+function isValidJson(j) {
+    try {
+        let json = JSON.parse(j);
+        json.media_list[0].media;
+        json.word_list[0].word;
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+
 // ファイルをセーブする
 document.getElementById("exportSettings").addEventListener("click", function () {
     chrome.storage.local.get("blockList", function (result) {
@@ -23,7 +36,14 @@ document.getElementById("importSettings").addEventListener("click", function () 
         let file = input.files[0];
         let reader = new FileReader();
         reader.addEventListener("load", function () {
+
+            // jsonが正しいかどうかを判定する
+            if (!isValidJson(reader.result)) {
+                alert("ファイルの整合性検証に失敗しました。");
+            }
+
             let json = JSON.parse(reader.result);
+            confirm(`ブロックリストを${file.name}で上書きしますか？`) &&
             chrome.storage.local.set({
                 blockList: JSON.stringify(json)
             });
